f091bc8c390b1e50f7d791195b03da91566372e0
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2019-12-12 22:50:50 -0800

    Added functionality to pull all comments from each queried submission and store them in Azure "comments" storage table

--- redditmends/modules/azure_storage_handler.py
+++ redditmends/modules/azure_storage_handler.py
@@ -14,11 +14,41 @@ class AzureStorageHandler():
 			print(ex)
 
 	def insert_submission_entry(self, entry):
-		task = Entity()
-		task.PartitionKey = entry.subreddit
-		task.RowKey = entry.id
-		task.author = entry.author
-		task.post_date = entry.post_date
-		task.flair = entry.flair
-		task.title = entry.title
-		self.table_service.insert_entity('submissions', task)
+		submission = Entity()
+
+		submission.PartitionKey = entry.subreddit
+		submission.RowKey = entry.id
+		submission.author = entry.author
+		submission.post_date = entry.post_date
+		submission.flair = entry.flair
+		submission.title = entry.title
+
+		self.table_service.insert_entity('submissions', submission)
+
+	def insert_comment_entry(self, entry):
+		comment = Entity()
+
+		comment.PartitionKey = entry.link_id
+		comment.RowKey = entry.id
+		comment.author = entry.author
+		comment.body = entry.body
+		comment.num_comments = entry.num_comments
+		comment.post_date = entry.post_date
+		comment.parent_id = entry.parent_id
+		comment.score = entry.score
+		comment.flair = entry.flair
+		comment.subreddit = entry.subreddit
+		comment.subreddit_id = entry.subreddit_id
+		comment.total_awards_received = entry.total_awards_received
+
+		self.table_service.insert_entity('comments', comment)
+
+	def insert_recommendation_entry(self, entry):
+		recommendation = Entity()
+
+		recommendation.PartitionKey = entry.subreddit
+		recommendation.RowKey = entry.id
+		recommendation.flair = entry.flair
+		recommendation.title = entry.title
+
+		self.table_service.insert_entity('recommendations', recommendation)
--- redditmends/redditmends_bot.py
+++ redditmends/redditmends_bot.py
@@ -1,4 +1,5 @@
 import praw
+from azure.common import AzureConflictHttpError
 from datetime import datetime, timedelta
 
 from modules.pushshift_handler import PushshiftHandler
@@ -7,6 +8,7 @@ from modules.inbox_handler import InboxHandler
 from modules.azure_storage_handler import AzureStorageHandler
 from accounts.reddit_account_info import accounts
 from models.reddit_submission_model import RedditSubmissionModel
+from models.reddit_comment_model import RedditCommentModel
 
 class RedditmendsBot():
 	def __init__(self, username):
@@ -20,19 +22,43 @@ class RedditmendsBot():
 		self.pushshift_handler = PushshiftHandler()
 		self.storage_account = AzureStorageHandler()
 
-	def run(self):
+	def run(self, submission_params):
 		# Initializes praw reddit instance
 		self.start_time = datetime.now()
 		# unread_messages = InboxHandler.read_inbox(self.reddit)
-		comments = self.pushshift_handler.fetch_comments(params=["q=hello", "limit=10"])
-		submissions_data = self.pushshift_handler.fetch_submissions(params=["subreddit=buildapcsales", "link_flair_text=CPU", "limit=10"])
 
-		for sub in submissions_data:
+		#TODO: fix queries for submissions (flair, etc.) --> make sure it's matching up
+		#TODO: only fetch submissions that we haven't seen yet
+		submissions = self.pushshift_handler.fetch_submissions(params=submission_params)
+
+		for sub in submissions:
+			# Add submission entry for database
 			submission = RedditSubmissionModel()
 			submission.parse_submission_data(sub)
-			self.storage_account.insert_submission_entry(submission)
+
+			try:
+				self.storage_account.insert_submission_entry(submission)
+			except AzureConflictHttpError as error:
+				print(error)
+				print(f"The submission entry with id =  %s already exists in the database. Continuing..." % submission.id)
+
+
+			# Add each comment for current submission into database
+			submission_comments = self.pushshift_handler.fetch_comments(params=["link_id=" + submission.id])
+
+			for com in submission_comments:
+				comment = RedditCommentModel()
+				comment.parse_comment_data(com)
+
+				try:
+					self.storage_account.insert_comment_entry(comment)
+				except AzureConflictHttpError as error:
+					print(error)
+					print(f"The comment entry with id =  %s already exists in the database. Continuing..." % comment.id)
+
 
 
 if __name__ == "__main__":
 	bot = RedditmendsBot("redditmends_bot")
-	bot.run()
+	#TODO: allow entry of parameters with spaces
+	bot.run(["subreddit=BuyItForLife", "title=[Request]", "num_comments=>1", "limit=10"]) # use ids here, seems to work for submissions - https://www.reddit.com/r/pushshift/comments/b3gvye/query_for_a_given_post_id/
