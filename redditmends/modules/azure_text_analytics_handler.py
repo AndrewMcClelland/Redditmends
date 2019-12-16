47850e6c3b7185313b4a4f5311e8659e080442b9
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2019-12-15 18:42:06 -0800

    Added module to use Azure Text Analytics (detect languages, sentiment analysis, keyword extraction, entity identifier)

---
+++ redditmends/modules/azure_text_analytics_handler.py
@@ -0,0 +1,74 @@
+#https://docs.microsoft.com/en-us/azure/cognitive-services/text-analytics/quickstarts/python
+
+import requests
+from pprint import pprint
+
+class TextAnalyticsHandler():
+	def __init__(self, kv):
+		self.key = kv.get_keyvault_secret("textAnalytics-key")
+		self.endpoint = kv.get_keyvault_secret("textAnalytics-endpoint")
+
+	def get_languages(self, texts):
+		api_url = self.endpoint + "/text/analytics/v2.1/languages"
+
+		id_count = 0
+
+		documents = {"documents": []}
+		for text in texts:
+			doc = {"id": f"{id_count}", "text": f"{text}"}
+			documents["documents"].append(doc)
+			id_count += 1
+
+		return self.__send_text_analytics_request(api_url, documents)
+
+	def get_sentiment(self, texts):
+		api_url = self.endpoint + "/text/analytics/v2.1/sentiment"
+
+		# Need to get languages for texts to hit keyphrase endpoint
+		texts_langs = self.get_languages(texts)
+
+		documents = {"documents": []}
+		for text in texts_langs["documents"]:
+			text_id = text["id"]
+			text_string = texts[int(text_id)]
+			language = text["detectedLanguages"][0]["iso6391Name"]
+			doc = {"id": f"{text_id}", "language": f"{language}", "text": f"{text_string}"}
+			documents["documents"].append(doc)
+
+		return self.__send_text_analytics_request(api_url, documents)
+
+	def get_key_phrases(self, texts):
+		api_url = self.endpoint + "/text/analytics/v2.1/keyphrases"
+
+		# Need to get languages for texts to hit keyphrase endpoint
+		texts_langs = self.get_languages(texts)
+
+		documents = {"documents": []}
+		for text in texts_langs["documents"]:
+			text_id = text["id"]
+			text_string = texts[int(text_id)]
+			language = text["detectedLanguages"][0]["iso6391Name"]
+			doc = {"id": f"{text_id}", "language": f"{language}", "text": f"{text_string}"}
+			documents["documents"].append(doc)
+
+		return self.__send_text_analytics_request(api_url, documents)
+
+	def get_entities(self, texts):
+		api_url = self.endpoint + "/text/analytics/v2.1/entities"
+
+		id_count = 0
+
+		documents = {"documents": []}
+		for text in texts:
+			doc = {"id": f"{id_count}", "text": f"{text}"}
+			documents["documents"].append(doc)
+			id_count += 1
+
+		return self.__send_text_analytics_request(api_url, documents)
+
+	def __send_text_analytics_request(self, url, body):
+		headers = {"Ocp-Apim-Subscription-Key": self.key}
+		response = requests.post(url, headers=headers, json=body)
+		results = response.json()
+		pprint(results)
+		return results
--- redditmends/redditmends_bot.py
+++ redditmends/redditmends_bot.py
@@ -2,12 +2,12 @@ import praw
 from azure.common import AzureConflictHttpError
 from datetime import datetime, timedelta
 
-from modules.keyvault_handler import KeyVaultHandler
-from modules.pushshift_handler import PushshiftHandler
-from modules.reddit_handler import RedditHandler
-from modules.inbox_handler import InboxHandler
+from modules.azure_keyvault_handler import KeyVaultHandler
+from modules.azure_text_analytics_handler import TextAnalyticsHandler
 from modules.azure_storage_handler import AzureStorageHandler
-from accounts.reddit_account_info import accounts
+from modules.pushshift_handler import PushshiftHandler
+from modules.reddit_praw_handler import RedditHandler
+from modules.reddit_inbox_handler import InboxHandler
 from models.reddit_submission_model import RedditSubmissionModel
 from models.reddit_comment_model import RedditCommentModel
 
@@ -16,8 +16,9 @@ class RedditmendsBot():
 
 		self.keyvault_handler = KeyVaultHandler("https://redditmends-kv.vault.azure.net/")
 		self.reddit = RedditHandler(self.keyvault_handler)
-		self.pushshift_handler = PushshiftHandler()
+		self.pushshift = PushshiftHandler()
 		self.storage_account = AzureStorageHandler(self.keyvault_handler)
+		self.text_analytics = TextAnalyticsHandler(self.keyvault_handler)
 
 	def run(self, submission_params):
 		# Initializes praw reddit instance
@@ -26,36 +27,41 @@ class RedditmendsBot():
 
 		#TODO: fix queries for submissions (flair, etc.) --> make sure it's matching up
 		#TODO: only fetch submissions that we haven't seen yet
-		submissions = self.pushshift_handler.fetch_submissions(params=submission_params)
+		submissions = self.pushshift.fetch_submissions(params=submission_params)
 
 		for sub in submissions:
 			# Add submission entry for database
 			submission = RedditSubmissionModel()
 			submission.parse_submission_data(sub)
 
-			try:
-				self.storage_account.insert_submission_entry(submission)
-			except AzureConflictHttpError as error:
-				print(error)
-				print(f"The submission entry with id =  %s already exists in the database. Continuing..." % submission.id)
-
+			# try:
+			# 	self.storage_account.insert_submission_entry(submission)
+			# except AzureConflictHttpError as error:
+			# 	print(error)
+			# 	print(f"The submission entry with id =  %s already exists in the database. Continuing..." % submission.id)
 
 			# Add each comment for current submission into database
-			submission_comments = self.pushshift_handler.fetch_comments(params=["link_id=" + submission.id])
+			submission_comments = self.pushshift.fetch_comments(params=["link_id=" + submission.id, "size=500"])
+
+			comment_text = []
 
 			for com in submission_comments:
 				comment = RedditCommentModel()
 				comment.parse_comment_data(com)
+				comment_text.append(comment.body)
 
-				try:
-					self.storage_account.insert_comment_entry(comment)
-				except AzureConflictHttpError as error:
-					print(error)
-					print(f"The comment entry with id =  %s already exists in the database. Continuing..." % comment.id)
+			self.text_analytics.get_key_phrases(comment_text)
+
+				# try:
+				# 	self.storage_account.insert_comment_entry(comment)
+				# except AzureConflictHttpError as error:
+				# 	print(error)
+				# 	print(f"The comment entry with id =  %s already exists in the database. Continuing..." % comment.id)
 
 
 
 if __name__ == "__main__":
 	bot = RedditmendsBot("redditmends_bot")
 	#TODO: allow entry of parameters with spaces
-	bot.run(["subreddit=BuyItForLife", "title=[Request]", "num_comments=>1", "limit=10"]) # use ids here, seems to work for submissions - https://www.reddit.com/r/pushshift/comments/b3gvye/query_for_a_given_post_id/
+	# bot.run(["subreddit=BuyItForLife", "title=[Request]", "num_comments=>1", "limit=10"]) # use ids here, seems to work for submissions - https://www.reddit.com/r/pushshift/comments/b3gvye/query_for_a_given_post_id/
+	bot.run(["ids=duv1bf"])
