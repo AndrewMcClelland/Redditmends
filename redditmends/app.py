975d6953ef34aa40bd8fe14b86a3409254b15069
Andrew McClelland <andrew.m.mcclelland@gmail.com>
2020-02-12 21:34:51 -0800

    Adding 'count' as a parameter in API query

--- redditmends/app.py
+++ redditmends/app.py
@@ -1,8 +1,7 @@
 import flask
 from flask import request, jsonify
 
-from redditmends_bot import RedditmendsBot
-from redditmends_bot import CommentQueryMethod
+from redditmends_bot import RedditmendsBot, CommentQueryMethod
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -20,5 +19,10 @@ def recommendations():
     else:
         return "Error: No term field provided. Please specify an id."
 
-    bot.run(search_term = term, comment_query_method = CommentQueryMethod.PRAW, num_top_recommendations = 5)
+    if 'count' in request.args:
+        count = int(request.args['count'])
+    else:
+        return "Error: No term count provided. Please specify an id."
+
+    bot.run(search_term = term,  num_top_recommendations = count, comment_query_method = CommentQueryMethod.PRAW)
     return "<h1>Redditmends bot</h1><p>{0}</p>".format(bot.result)
